cfeee1c0439fdf2637f1abee875e8333e1bf056b
Monika Eftimova <meftimova@uni-sofia.bg>
2015-11-12 10:29:35 +0200

    Add binary

---
+++ week2/homework/solutions/binary.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (string-reverse str)
+  (define (iter result count)
+    (cond
+      [(< count 0) result]
+      [else (iter (string-append result (~a (string-ref str count))) (- count 1))]))
+  (iter "" (- (string-length str) 1)))
+
+(define (to-binary-string n)
+  (define (iter result number)
+    (cond
+      [(= number 0) (string-reverse result)]
+      [else (iter (string-append result (~a (remainder number 2))) (quotient number 2))]))
+  (iter "" n))
+
+(define (from-binary-string binary-str)
+  (define (iter result number count)
+    (cond
+      [(= number 0) result]
+      [else (iter (+ result (* (expt 2 count) (remainder number 10))) (quotient number 10) (add1 count))]))
+
+  (iter 0 (string->number binary-str) 0))
